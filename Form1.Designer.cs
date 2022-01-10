c797a0ec8cff08f08e8bd24554f5f42e0fd26f1d
derek221028 <Shikaimu2008@126.com>
2022-01-10 14:09:18 +0800

    修改World

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,9 +37,9 @@ namespace HelloWord
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(64, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.Size = new System.Drawing.Size(74, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "HelloWord";
+            this.label1.Text = "HelloWorld";
             // 
             // Form1
             // 
