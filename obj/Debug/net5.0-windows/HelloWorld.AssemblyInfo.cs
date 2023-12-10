fcda576bbfe3241dc8a735e4ce97feb8b08f3755
derek221028 <Shikaimu2008@126.com>
2023-12-11 00:16:06 +0800

    自动生成文件同步

--- obj/Debug/net5.0-windows/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net5.0-windows/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+038df616f938b357131c005586febb4868a8bade")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
